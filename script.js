2b1c1d640f1ae5a6b089abcadee77495b3bf308e
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-05-17 11:17:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ let squares = document.querySelectorAll(".square")
 
 for (let i = 0; i < squares.length; i++) {
 	squares[i].addEventListener("mouseover",()=>{
-		squares[i].style.backgroundColor = "#6F4E37"
+		//squares[i].style.backgroundColor = "#E6E6FA"
+		for (let j = 0; j < squares.length; j++) {
+			if(j!=i){
+				squares[j].style.backgroundColor = "#6F4E37"
+			}
+		}
 	})
 	squares[i].addEventListener("mouseout",()=>{
-		squares[i].style.backgroundColor = " #E6E6FA"
+		//squares[i].style.backgroundColor = "#6F4E37"
+		for (let j = 0; j < squares.length; j++) {
+			if(j!=i){
+				squares[j].style.backgroundColor = "#E6E6FA"
+			}
+		}
 	})
 }
